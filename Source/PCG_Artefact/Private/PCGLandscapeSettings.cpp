0dc518aaf59e99f481fa6d747b36ab84ef4345ff
B034418M <b034418m@student.staffs.ac.uk>
2024-11-01 14:53:14 +0000

    Curb integration 1

--- Source/PCG_Artefact/Private/PCGLandscapeSettings.cpp
+++ Source/PCG_Artefact/Private/PCGLandscapeSettings.cpp
@@ -3,6 +3,9 @@
 
 #include "PCGLandscapeSettings.h"
 
+#include "Landscape.h"
+#include "LandscapeSplineSegment.h"
+
 // Sets default values
 APCGLandscapeSettings::APCGLandscapeSettings()
 {
@@ -10,6 +13,64 @@ APCGLandscapeSettings::APCGLandscapeSettings()
 	PrimaryActorTick.bCanEverTick = true;
 
 }
+
+#if WITH_EDITOR
+void APCGLandscapeSettings::UpdateLandscapeSplineMeshes()
+{
+	if(!_Landscape) {UE_LOG(LogTemp, Warning, TEXT("NO LANDSCAPE")); return;}
+
+	//_Landscape->PreEditUndo();
+
+	_SplineSegments.Empty();
+
+	TArray<TWeakObjectPtr<ULandscapeSplineSegment>> Segments;
+	for(TObjectIterator<ULandscapeSplineSegment> Itr; Itr; ++Itr)
+	{
+		Segments.Add(TWeakObjectPtr<ULandscapeSplineSegment>(*Itr));
+	}
+	for (auto LandscapeSplineSegment : Segments)
+	{
+		if(LandscapeSplineSegment.IsValid())
+		{
+			_SplineSegments.Add(*LandscapeSplineSegment);
+		}
+	}
+
+	for (auto LandscapeSplineSegment : _SplineSegments)
+	{
+		LandscapeSplineSegment->SplineMeshes.Empty();
+
+		if(!_ShowLCurb && !_ShowRCurb)
+		{
+			LandscapeSplineSegment->SplineMeshes.Add(CreateMeshEntry(_RoadMesh));
+		}
+		else if(_ShowLCurb && !_ShowRCurb)
+		{
+			LandscapeSplineSegment->SplineMeshes.Add(CreateMeshEntry(_RoadMeshL));
+		}
+		else if(!_ShowLCurb && _ShowRCurb)
+		{
+			LandscapeSplineSegment->SplineMeshes.Add(CreateMeshEntry(_RoadMeshR));
+		}
+		else if(_ShowLCurb && _ShowRCurb)
+		{
+			LandscapeSplineSegment->SplineMeshes.Add(CreateMeshEntry(_RoadMeshLR));
+		}
+	}
+	
+	_Landscape->PostEditMove(true);
+}
+
+FLandscapeSplineMeshEntry APCGLandscapeSettings::CreateMeshEntry(UStaticMesh* Mesh)
+{
+	FLandscapeSplineMeshEntry data;
+
+	// Change other data here!
+	data.Mesh = Mesh;
+	
+	return data;
+}
+#endif
 
 // Called when the game starts or when spawned
 void APCGLandscapeSettings::BeginPlay()
--- Source/PCG_Artefact/Public/PCGLandscapeSettings.h
+++ Source/PCG_Artefact/Public/PCGLandscapeSettings.h
@@ -6,6 +6,9 @@
 #include "GameFramework/Actor.h"
 #include "PCGLandscapeSettings.generated.h"
 
+class ALandscape;
+class ULandscapeSplineSegment;class ULandscapeComponent;
+
 UCLASS()
 class PCG_ARTEFACT_API APCGLandscapeSettings : public AActor
 {
@@ -15,9 +18,42 @@ public:
 	// Sets default values for this actor's properties
 	APCGLandscapeSettings();
 
+	UFUNCTION(CallInEditor, Category = "Spline Component")
+	void UpdateLandscapeSplineMeshes();
+
+	UFUNCTION()
+	FLandscapeSplineMeshEntry CreateMeshEntry(UStaticMesh* Mesh);
+
+	//UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Spline Component")
+	
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool _ShowLCurb;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool _ShowRCurb;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TObjectPtr<UStaticMesh> _RoadMesh;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TObjectPtr<UStaticMesh> _RoadMeshL;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TObjectPtr<UStaticMesh> _RoadMeshR;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TObjectPtr<UStaticMesh> _RoadMeshLR;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TObjectPtr<ALandscape> _Landscape;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TArray<TObjectPtr<ULandscapeSplineSegment>> _SplineSegments;
 
 public:	
 	// Called every frame
